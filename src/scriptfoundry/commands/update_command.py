44d476ee853de0ddbb18e0fd8c166eb8d23ebdfe
katzy687 <katzy687@gmail.com>
2022-10-14 05:31:35 +0300

    added context manager around directory change action

--- src/scriptfoundry/commands/update_command.py
+++ src/scriptfoundry/commands/update_command.py
@@ -1,3 +1,4 @@
+import contextlib
 import os
 
 import click
@@ -7,6 +8,16 @@ from shellfoundry.exceptions import FatalError
 from scriptfoundry.commands.pack_command import pack_script
 from scriptfoundry.utilities import check_connectivity
 from scriptfoundry.utilities.api_generator import AutomationApiGenerator
+
+
+@contextlib.contextmanager
+def pushd_context(new_dir):
+    previous_dir = os.getcwd()
+    os.chdir(new_dir)
+    try:
+        yield
+    finally:
+        os.chdir(previous_dir)
 
 
 class ScriptUpdateExecutor:
@@ -23,8 +34,8 @@ class ScriptUpdateExecutor:
             try:
                 # need to change dir before uploading, otherwise full path gets stored in cloudshell and causes issues
                 dist_dir = os.path.join(zip_details.script_dir_path, "dist")
-                os.chdir(dist_dir)
-                self._api.UpdateScript(zip_details.script_name, zip_details.zip_file_name)
+                with pushd_context(dist_dir):
+                    self._api.UpdateScript(zip_details.script_name, zip_details.zip_file_name)
             except CloudShellAPIError as e:
                 if e.code == "100":
                     raise FatalError(f"'{zip_details.script_name}' not on Cloudshell. Upload manually the first time.")
--- tests/test_update.py
+++ tests/test_update.py
@@ -6,26 +6,26 @@ from shellfoundry.exceptions import FatalError
 from scriptfoundry.commands.update_command import ScriptUpdateExecutor
 
 
-@fixture
-def updater():
-    return ScriptUpdateExecutor()
-
-
 def _get_script_dir(script_dir_name: str):
     curr_dir = os.getcwd()
     script_full_path = os.path.join(curr_dir, "test-assets", "zip-test-scripts", script_dir_name)
     return script_full_path
 
 
-def test_simple_update(updater):
+@fixture
+def simple_script_dir():
+    return _get_script_dir("simple_script")
+
+
+def test_simple_update(simple_script_dir):
     try:
-        updater.update_script(script_dir_path=_get_script_dir("simple_script"))
+        ScriptUpdateExecutor().update_script(script_dir_path=simple_script_dir)
     except FatalError:
         pass
 
 
-def test_update_different_name(updater):
+def test_update_different_name(simple_script_dir):
     try:
-        updater.update_script(script_name="dev_simple_test", script_dir_path=_get_script_dir("simple_script"))
+        ScriptUpdateExecutor().update_script(script_name="dev_simple_test", script_dir_path=simple_script_dir)
     except FatalError:
         pass
